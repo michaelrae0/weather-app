7687e91e6f5bbaccf33ca60562361dae0f39eb6d
mdrae <mdrae0@gmail.com>
2018-12-25 08:32:53 -0600

    Updates

--- precache-manifest.00c9460442a6acc78da815e2b3d5272b.js
+++ precache-manifest.00c9460442a6acc78da815e2b3d5272b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "74a98bc462d9694df61a",
+    "revision": "b82c2015ae4f0c10a88b",
     "url": "/weather-app/static/css/main.6f72b76a.chunk.css"
   },
   {
-    "revision": "74a98bc462d9694df61a",
-    "url": "/weather-app/static/js/main.74a98bc4.chunk.js"
+    "revision": "b82c2015ae4f0c10a88b",
+    "url": "/weather-app/static/js/main.b82c2015.chunk.js"
   },
   {
     "revision": "31d9b4a0954dd3bb929a",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/weather-app/static/media/pattern.e78b5615.svg"
   },
   {
-    "revision": "7ca86ba3c13add2db25e964e3210c958",
+    "revision": "60b718d68b8415b8cfd86a982966858b",
     "url": "/weather-app/index.html"
   }
 ];
